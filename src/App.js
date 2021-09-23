937a174e6438d5f1a2be524a5271e953454c9b0a
Eddievin <thokerunga200@gmail.com>
2021-09-23 20:47:42 +0300

    feat: add cards and connect to api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,42 @@
-/* eslint-disable quotes */
-/* eslint-disable react/react-in-jsx-scope */
-/* eslint-disable react/prefer-stateless-function */
-import React from 'react';
+import React from "react";
+
+import { Cards, CountryPicker, Chart } from "./components";
+import { fetchData } from "./api/";
 import styles from "./App.module.css";
+
 import image from "./images/image.png";
 
 class App extends React.Component {
+  state = {
+    data: {},
+    country: "",
+  };
+
+  async componentDidMount() {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
+
+  handleCountryChange = async (country) => {
+    const data = await fetchData(country);
+
+    this.setState({ data, country: country });
+  };
+
   render() {
+    const { data, country } = this.state;
+
     return (
       <div className={styles.container}>
-        <img className={styles.image} src={image} alt="COVID-19" />
+        <div className={styles.containeritems}>
+          <img className={styles.image} src={image} alt="COVID-19" />
+
+          <Cards data={data} />
+
+          <CountryPicker handleCountryChange={this.handleCountryChange} />
+        </div>
+        <Chart data={data} country={country} />
       </div>
     );
   }
