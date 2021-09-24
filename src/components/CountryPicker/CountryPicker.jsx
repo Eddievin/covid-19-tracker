4353f5250aa1d4a03f4c978480aca45f737974a3
Eddievin <thokerunga200@gmail.com>
2021-09-24 22:24:25 +0300

    feat: add country picker

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,10 +1,30 @@
-import React from "react";
-import { FormControl } from "@material-ui/core";
+import React, { useState, useEffect } from 'react';
+import { NativeSelect, FormControl } from '@material-ui/core';
 
-import styles from "./CountryPicker.module.css";
+import { fetchCountries } from '../../api';
 
-const Countries = () => {
-  return <FormControl className={styles.formControl}></FormControl>;
+import styles from './CountryPicker.module.css';
+
+const Countries = ({ handleCountryChange }) => {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setCountries(await fetchCountries());
+    };
+
+    fetchAPI();
+  }, []);
+
+  return (
+    <FormControl className={styles.formControl}>
+      <p>Please select a country and see the graph</p>
+      <NativeSelect defaultValue="" onChange={(e) => handleCountryChange(e.target.value)}>
+        <option value="">United States</option>
+        {countries.map((country, i) => <option key={i} value={country}>{country}</option>)}
+      </NativeSelect>
+    </FormControl>
+  );
 };
 
 export default Countries;
